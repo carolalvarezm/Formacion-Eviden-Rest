5176b34b53c8acfd13e6f4b3e1b8af75ac99762b
Carolina Candelaria Alvarez Martin <carolina.alvarezm@eviden.com>
2024-04-16 17:14:51 +0100

    Agregando los proyectos al ejemplo de OpenApi

---
+++ EjemploOpenAPI/src/main/java/es/a926666/EjemploOpenAPI/App.java
@@ -0,0 +1,13 @@
+package es.a926666.EjemploOpenAPI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
